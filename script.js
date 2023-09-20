e063c14385d6ef579d438145406340a5db2e0842
Bhawesh Choudhary <bhaweshchoudhary600@gmail.com>
2023-09-20 06:21:07 +0530

    Modified Files

--- script.js
+++ script.js
@@ -12,11 +12,24 @@
       }
 
 // form submission
-var sidemenu = document.getElementById("sidemenu");
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbx39RcoNZcpKHY1npVi-oUuKkqdvTgAKQAGsS77BgOpsf4rtPtw-mH96miWcGXKws7o/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
 
-      function openmenu(){
-        sidemenu.style.right = "0";
-      }
-      function closemenu(){
-        sidemenu.style.right = "-200px";
-      }
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent Successfully !"
+        setTimeout(function(){
+          msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
+
+      .catch(error => console.error('Error!', error.message))
+  })
+
+
+
